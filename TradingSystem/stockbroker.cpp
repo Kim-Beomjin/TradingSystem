643d3b6b799dd3ab550cbe94460e6a1e50bcbca8
yunzhen-kim <yunzhen.kim@gmail.com>
2025-07-28 14:00:31 +0900

    fix login to return bool
    - fix login function of each Broker class
    - fix Test codes for login
    
    Signed-off-by: yunzhen-kim <yunzhen.kim@gmail.com>

--- TradingSystem/stockbroker.cpp
+++ TradingSystem/stockbroker.cpp
@@ -1,19 +1,33 @@
 #include "kiwer_api.cpp"
 #include "nemo_api.cpp"
 #include <exception>
+#include <sstream>
 
 interface StockBroker {
-	virtual void login(std::string ID, std::string password) = 0;
+	virtual bool login(std::string ID, std::string password) = 0;
 	virtual void buy(std::string stockCode, int count, int price) = 0;
 	virtual void sell(std::string stockCode, int count, int price) = 0;
 	virtual int currentPrice(std::string stockCode) = 0;
 };
 
-class KiwerStockBroker : public StockBroker, public KiwerAPI {
+class KiwerStockBroker : public StockBroker {
 public:
-	void login(std::string ID, std::string password) override {
-		login(ID, password);
-	}	
+	bool login(std::string ID, std::string password) override {
+		std::ostringstream oss = {};
+		auto oldCoutStreamBuf = std::cout.rdbuf();
+		std::cout.rdbuf(oss.rdbuf());
+
+		kiwerAPI.login(ID, password);
+		if (oss.str().find("login success") != std::string::npos) {
+			std::cout.rdbuf(oldCoutStreamBuf);
+			std::cout << "Kiwer login success\n";
+			return true;
+		} else {
+			std::cout.rdbuf(oldCoutStreamBuf);
+			std::cout << "Kiwer login failed\n";
+			return false;
+		}
+	}
 
 	void buy(std::string stockCode, int count, int price) override {
 
@@ -26,12 +40,27 @@ public:
 	int currentPrice(std::string stockCode) override {
 		return 0;
 	}
+private:
+	KiwerAPI kiwerAPI;
 };
 
-class NemoStockBroker : public StockBroker, public NemoAPI {
+class NemoStockBroker : public StockBroker {
 public:
-	void login(std::string ID, std::string password) override {
-		certification(ID, password);
+	bool login(std::string ID, std::string password) override {
+		std::ostringstream oss = {};
+		auto oldCoutStreamBuf = std::cout.rdbuf();
+		std::cout.rdbuf(oss.rdbuf());
+
+		nemoAPI.certification(ID, password);
+		if (oss.str().find("login GOOD") != std::string::npos) {
+			std::cout.rdbuf(oldCoutStreamBuf);
+			std::cout << "Nemo login success\n";
+			return true;
+		} else {
+			std::cout.rdbuf(oldCoutStreamBuf);
+			std::cout << "Nemo login failed\n";
+			return false;
+		}
 	}
 
 	void buy(std::string stockCode, int count, int price) override {
@@ -45,6 +74,8 @@ public:
 	int currentPrice(std::string stockCode) override {
 		return 0;
 	}
+private:
+	NemoAPI nemoAPI;
 };
 
 class AutoTradingSystem {
@@ -79,8 +110,8 @@ public:
 		if (password.empty()) {
 			return false;
 		}
-		stockBroker->login(ID, password);
-		return true;
+
+		return stockBroker->login(ID, password);
 	}
 	void buy(std::string stockCode, int count, int price);
 	int currentPrice(std::string stockCode)
