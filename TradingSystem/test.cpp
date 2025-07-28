1e0ebd26763e3770b0dd1c7924e625fe8f6e8a9c
yunzhen-kim <yunzhen.kim@gmail.com>
2025-07-28 09:58:13 +0900

    [feature] make "AutoTradingSystem app"
    make below classes
     - AutoTradingSystem
     - StockBroker interface
     - KiwerStockBroker
     - NemoStockBroker
     - MockStockBroker to test.cpp
    
    Signed-off-by: yunzhen-kim <yunzhen.kim@gmail.com>

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -1,4 +1,5 @@
 #include "gmock/gmock.h"
+#include "stockbroker.cpp"
 
 using namespace std;
 using namespace testing;
@@ -6,6 +7,12 @@ using namespace testing;
 class AutoTradingFixture : public Test {
 public:
 	AutoTradingSystem app;
+};
+
+class MockStockBroker : public StockBroker {
+	MOCK_METHOD(void, login, (string ID, string password), (override));
+	MOCK_METHOD(void, buy, (string stockCode, int count, int price), (override));
+	MOCK_METHOD(int, currentPrice, (string stockCode), (override));
 };
 
 TEST_F(AutoTradingFixture, ThrowInvalidSelection) {
@@ -18,3 +25,4 @@ TEST_F(AutoTradingFixture, ThrowInvalidSelection) {
 			string{ "Invalid stock broker selection" });
 	}
 }
+
