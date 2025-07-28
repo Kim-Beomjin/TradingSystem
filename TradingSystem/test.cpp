8061a00323a248ea06b5d7be08453410131dc68e
beom-jin.kim <beom-jin.kim@samsung.com>
2025-07-28 09:23:56 +0900

    [UT1] Throw invalid selection

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -1,2 +1,20 @@
 #include "gmock/gmock.h"
 
+using namespace std;
+using namespace testing;
+
+class AutoTradingFixture : public Test {
+public:
+	AutoTradingSystem app;
+};
+
+TEST_F(AutoTradingFixture, ThrowInvalidSelection) {
+	try {
+		app.selectStockBrocker("Invalid");
+		FAIL();
+	}
+	catch (runtime_error& e) {
+		EXPECT_EQ(string{ e.what() },
+			string{ "Invalid stock broker selection" });
+	}
+}
