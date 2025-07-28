de4a73cbe2d9679c0536f56a0ae16e5b34110b19
yunzhen-kim <yunzhen.kim@gmail.com>
2025-07-28 09:58:13 +0900

    make "AutoTradingSystem app"
    make below classes
     - AutoTradingSystem
     - StockBroker interface
     - KiwerStockBroker
     - NemoStockBroker
     - MockStockBroker to test.cpp
    
    add SelectKiwerStockBrokerAndBuyStock TC to implement next step
    
    Signed-off-by: yunzhen-kim <yunzhen.kim@gmail.com>

---
+++ TradingSystem/stockbroker.cpp
@@ -0,0 +1,76 @@
+#include "kiwer_api.cpp"
+#include "nemo_api.cpp"
+#include <exception>
+
+interface StockBroker {
+	virtual void login(std::string ID, std::string password) = 0;
+	virtual void buy(std::string stockCode, int count, int price) = 0;
+	virtual void sell(std::string stockCode, int count, int price) = 0;
+	virtual int currentPrice(std::string stockCode) = 0;
+};
+
+class KiwerStockBroker : public StockBroker, public KiwerAPI {
+public:
+	void login(std::string ID, std::string password) override {
+
+	}
+
+	void buy(std::string stockCode, int count, int price) override {
+
+	}
+
+	void sell(std::string stockCode, int count, int price) override {
+
+	}
+
+	int currentPrice(std::string stockCode) override {
+		return 0;
+	}
+private:
+	KiwerAPI kiwerAPI;
+};
+
+class NemoStockBroker : public StockBroker, public NemoAPI {
+public:
+	void login(std::string ID, std::string password) override {
+
+	}
+
+	void buy(std::string stockCode, int count, int price) override {
+
+	}
+
+	void sell(std::string stockCode, int count, int price) override {
+
+	}
+
+	int currentPrice(std::string stockCode) override {
+		return 0;
+	}
+private:
+	NemoAPI nemoAPI;
+};
+
+class AutoTradingSystem {
+public:
+	void selectStockBrocker(std::string brokerName) {
+		if (brokerName == "Kiwer") {
+			stockBroker = new KiwerStockBroker();
+		} else if (brokerName == "Nemo") {
+			stockBroker = new NemoStockBroker();
+		} else {
+			throw std::runtime_error("Invalid stock broker selection");
+		}
+		stockBroker = nullptr;
+	}
+
+	void setStockBroker(StockBroker* broker) {
+		if (stockBroker != nullptr) {
+			delete stockBroker;
+		}
+		stockBroker = broker;
+	}
+
+private:
+	StockBroker* stockBroker;
+};
--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -1,4 +1,5 @@
 #include "gmock/gmock.h"
+#include "stockbroker.cpp"
 
 using namespace std;
 using namespace testing;
@@ -6,6 +7,12 @@ using namespace testing;
 class AutoTradingFixture : public Test {
 public:
 	AutoTradingSystem app;
+};
+
+class MockStockBroker : public StockBroker {
+	MOCK_METHOD(void, login, (string ID, string password), (override));
+	MOCK_METHOD(void, buy, (string stockCode, int count, int price), (override));
+	MOCK_METHOD(int, currentPrice, (string stockCode), (override));
 };
 
 TEST_F(AutoTradingFixture, ThrowInvalidSelection) {
@@ -17,4 +24,18 @@ TEST_F(AutoTradingFixture, ThrowInvalidSelection) {
 		EXPECT_EQ(string{ e.what() },
 			string{ "Invalid stock broker selection" });
 	}
+}
+
+TEST_F(AutoTradingFixture, SelectKiwerStockBrokerAndBuyStock) {
+	// TODO: Implement KiwerStockBroker's buy method
+	app.selectStockBrocker("Kiwer");
+	std::string stockCode = "test";
+	int count = 0;
+	int price = 0;
+
+	app.setStockBroker(&mockBroker);
+
+	app.buyNiceTiming(stockCode, count, price);
+
+	EXPECT_EQ(app.stockBroker->currentPrice(stockCode), price);
 }
